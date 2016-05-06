9e1259cc38d9be02a9e205c4d9dc0c2974395477
Xiaojian Deng <xjd001@gmail.com>
2016-05-06 14:11:03 -0700

    fix: Eliminate bugs with q0 and reset()

--- QLearner.py
+++ QLearner.py
@@ -1,7 +1,7 @@
 import random
 
 class QLearner(object):
-    def __init__(self, q0, actions, learning = 0.5, discount = 0.5, default_q = 0.1):
+    def __init__(self, actions, q0={}, learning = 0.5, discount = 0.5, default_q = 0.1):
         self.actions = actions
         self.learning = learning
         self.discount = discount
@@ -11,12 +11,14 @@ class QLearner(object):
         self.state0 = None
         self.action0 = None
         self.reward0 = None
-        self.default_q = 0.1    
+        self.default_q = default_q    
+    
+    def getQ(self,state,action):
+        if (state,action) not in self.Q:
+            self.Q[(state,action)] = self.default_q
+        return self.Q[(state,action)]
     
     def reset(self):
-        if (self.state0, self.action0, self.reward0) != (None, None, None):
-            oldQ = self.Q[(self.state0,self.action0)]
-            self.Q[(self.state0,self.action0)] = oldQ + self.learning*(self.reward0 - oldQ)
         (self.state0, self.action0, self.reward0) = (None, None, None)
     
     def pickAction(self, state):
@@ -24,15 +26,15 @@ class QLearner(object):
         (j,k) = (0,0)
         self.A = None
         while (k < len(self.actions)) & (j == 0):
-            if self.Q[(state,self.actions[0])] == self.Q[(state,self.actions[k])]:
+            if self.getQ(state,self.actions[0]) == self.getQ(state,self.actions[k]):
                 j = 1
             k += 1
         if j == 0:
             self.A = random.choice(self.actions)
         else:
             for i2 in self.actions:
-                if self.Q[(state,i2)] >= self.newQ:
-                    self.newQ = self.Q[(state,i2)]
+                if self.getQ(state,i2) >= self.newQ:
+                    self.newQ = self.getQ(state,i2)
                     self.A = i2
         return self.A
     
@@ -40,7 +42,7 @@ class QLearner(object):
         if newAction is not None:
             self.A = newAction
         if (self.state0, self.action0, self.reward0) != (None, None, None):
-            oldQ = self.Q[(self.state0,self.action0)]
+            oldQ = self.getQ(self.state0,self.action0)
             self.Q[(self.state0, self.action0)] = (1-self.learning)*oldQ + self.learning*(self.reward0 + self.discount*self.newQ)
         (self.state0, self.action0, self.reward0) = (state, self.A, reward)
         
